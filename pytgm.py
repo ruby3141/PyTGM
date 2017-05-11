6112c2245f3afe16c3cb9c228d3360be2fa7e21e
CDnX <ruby3141@gmail.com>
2017-05-11 16:48:00 +0900

    initial commit
    with some default structure and a easter egg

---
+++ pytgm.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+	import module.ctrl as ctrl
+	ctr = ctrl.controller()
+	ctr.run()
+else:
+	import sdl2
+	import sdl2.ext
+	import random
+	sdl2.ext.init()
+	window = sdl2.ext.Window("Triggered", size=(320, 371))
+	window.show()
+	RESOURCES = sdl2.ext.Resources(__file__, "./res")
+	factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+	sprite = factory.from_image(RESOURCES.get_path("triggered.png"))
+	spriterenderer = factory.create_sprite_render_system(window)
+	running=True
+	while running:
+		events=sdl2.ext.get_events()
+		for event in events:
+			if event.type == sdl2.SDL_QUIT:
+				running=False
+				break;
+		sprite.position = random.randrange(-5,6), random.randrange(-5,6)
+		spriterenderer.render(sprite)
+		sdl2.SDL_Delay(16)
+	window.hide()
